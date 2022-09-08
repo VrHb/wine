4e0f9fd0c7f7c38c8a19cdd04bd714fe811687ea
Victor Abdukarimov <viredmi@gmail.com>
2022-09-08 17:42:05 +0300

    Added jinja2 template

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import datetime
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+
+def get_correct_ending(years: int) -> str:
+    number = years % 10
+    if number == 1:
+        return f"{years} год"
+    elif number in range(5, 21):
+        return f"{years} лет"
+    elif number in range(2, 5):
+        return f"{years} года"
+    return f"{years} лет"
+
+
+start_company_year = datetime(year=1920, month=1, day=1).year
+company_age = datetime.now().year - start_company_year       
+
+rendered_page = template.render(
+    correct_company_age = get_correct_ending(company_age)      
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
