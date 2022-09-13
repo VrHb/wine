98c2bcc524573e75aa5000357e913cf2f9bb7eff
Viktor Abdukarimov <viredmi@gmail.com>
2022-09-13 10:10:01 +0300

    Fixed args default value, quotes, description, constants

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-DEFAULT_FILENAME = 'wines.xlsx'
+START_COMPANY_YEAR = 1920
 
 
 def get_correct_ending(years: int) -> str:
@@ -24,46 +24,45 @@ def get_correct_ending(years: int) -> str:
 def group_drinks(drinks: dict) -> dict:
     grouped_drinks = collections.defaultdict(list)
     for drink in drinks:
-        grouped_drinks[drink['Категория']].append(drink)
+        grouped_drinks[drink["Категория"]].append(drink)
     return grouped_drinks
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description="Описание программы"
+        description="Программа запускает локальный сервер вашего сайта"
     )
-    parser.add_argument('file', help="Имя файла или абсолютный путь к файлу")
+    parser.add_argument(
+        "-f",
+        "--file",
+        default="wines.xlsx",
+        help="Имя файла или абсолютный путь к файлу (По умолчанию: wines.xlsx)"
+    )
     args = parser.parse_args()
 
-    company_age = datetime.now().year - 1920       
+    company_age = datetime.now().year - START_COMPANY_YEAR       
 
-    try:
-        drinks = pandas.read_excel(
-            args.file, 
-            sheet_name='Лист1',
-            keep_default_na=False
-        )
-    except FileNotFoundError:
-        drinks = pandas.read_excel(
-            DEFAULT_FILENAME, 
-            sheet_name='Лист1',
-            keep_default_na=False
-        )
-    grouped_drinks = group_drinks(drinks.to_dict('record'))
+    drinks = pandas.read_excel(
+        args.file, 
+        sheet_name="Лист1",
+        keep_default_na=False
+    )
+
+    grouped_drinks = group_drinks(drinks.to_dict("record"))
     
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"])
     )
-    template = env.get_template('template.html')
+    template = env.get_template("template.html")
     rendered_page = template.render(
         grouped_drinks=grouped_drinks,
         correct_company_age=get_correct_ending(company_age)      
     )
-    with open('index.html', 'w', encoding="utf8") as file:
+    with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
 
 if __name__ == "__main__":
